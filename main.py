fe7053e09eba9541376849a79d2d8ed003d24b28
g1g0byte <gabriel.maccallum1@gmail.com>
2021-03-20 18:01:55 +0000

    gui functions but is very rough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv,random
+from tkinter import *
 
 def read_words_file():
 	words_list = {}
@@ -8,38 +9,8 @@ def read_words_file():
 			words_list.update({row[0]:row[1]})
 	return words_list
 
-def amount_of_words_input():
-	while True:
-		try:
-			amount_of_words = int(input("Enter amount of words to use: "))
-			if amount_of_words <= 0 or amount_of_words > 50:
-				print("Please enter a reasonable amount of words! (0 < amount <= 50)\n")
-				continue
-		
-		except ValueError:# Handle if input is not an integer
-			print("Please enter an amount of words!\n")
-			continue
-		else:
-			return amount_of_words
-
-def yes_no_input(message):
-	while True:
-		try:
-			user_input = int(input(message))
-			if user_input not in [1,0]:
-				print("Please enter 1 (yes) or 0 (no)!\n")
-				continue
-
-		except ValueError:	# Handle if input is not an integer
-			print("Please enter 1 (yes) or 0 (no)!\n")
-			continue
-		
-		if user_input == 1:
-			return True
-		else:
-			return False
-
-def generate_word_codes(amount_of_words):
+def create_passphrase(words_list,amount_of_words,should_space_words, should_capitalise, should_random_numbers):
+	# Create list of random word codes
 	word_codes=[]
 	for i in range(amount_of_words):
 		word_code=''
@@ -47,21 +18,17 @@ def generate_word_codes(amount_of_words):
 			roll = random.SystemRandom().randint(1, 6)
 			word_code += str(roll)
 		word_codes.append(word_code)
-	return word_codes
 
-def find_words_list(words_list,word_codes):
+	# Find words that match the generated word codes
 	selected_words=[]
 	for i in range(len(word_codes)):
 		# loop through each key in words_list to find matching word code
 		if word_codes[i] in words_list.keys():
 			selected_words.append(words_list[word_codes[i]])
 		else:
-			print("Problem occured with finding word in list")
+			print("Problem occured with finding a word in list")
 			quit()
-	return selected_words
 
-def create_passphrase(selected_words, should_space_words, should_capitalise, should_random_numbers):
-	amount_of_words = len(selected_words)
 	if (should_capitalise):
 		for i in range(amount_of_words):
 			selected_words[i] = selected_words[i].capitalize()
@@ -82,18 +49,42 @@ def create_passphrase(selected_words, should_space_words, should_capitalise, sho
 	passphrase=''
 	for i in range(amount_of_words):
 		passphrase += selected_words[i]+numbers[i]+space
+	
+	passphrase_label = Label(root,text=passphrase)
+	passphrase_label.grid()
 	return passphrase
 
 # main program
 words_list = read_words_file()
-#-------------------------------------------------
-amount_of_words = amount_of_words_input()
-should_space_words = yes_no_input("Do you want a space between words? (yes=1,no=0) : ")
-should_capitalise = yes_no_input("Do you want to capitalise each word? (yes=1,no=0) : ")
-should_random_numbers = yes_no_input("Do you want a random number at end of each word? (yes=1,no=0) : ")
-#-------------------------------------------------
-word_codes = generate_word_codes(amount_of_words)
-selected_words = find_words_list(words_list, word_codes)
-#-------------------------------------------------
-passphrase = create_passphrase(selected_words, should_space_words, should_capitalise, should_random_numbers)
-print("Passphrase:",passphrase)
+
+# tkinter code
+root = Tk()
+root.title("Passphrase generator")
+root.geometry("300x400") 
+
+amount_of_words_title = Label(root,text="Amount of words:")
+amount_of_words_inputfield = Entry(root,width=2)
+amount_of_words_inputfield.insert(0, 4)
+
+should_space_words = IntVar()
+space_words_checkbutton = Checkbutton(root,text="Space between words",variable=should_space_words)
+space_words_checkbutton.select()
+
+should_capitalise = IntVar()
+capitalise_checkbutton = Checkbutton(root,text="capitalise word",variable=should_capitalise)
+
+should_random_numbers = IntVar()
+random_numbers_checkbutton = Checkbutton(root,text="random numbers",variable=should_random_numbers)
+
+generate_button = Button(root,text="Generate Passphrase",
+command=lambda: create_passphrase(words_list,int(amount_of_words_inputfield.get()),should_space_words.get(),should_capitalise.get(), should_random_numbers.get()))
+
+
+generate_button.grid(row=0)
+amount_of_words_title.grid(row=1,column=0)
+amount_of_words_inputfield.grid(row=1,column=1)
+space_words_checkbutton.grid(row=2)
+capitalise_checkbutton.grid(row=3)
+random_numbers_checkbutton.grid(row=4)
+
+root.mainloop()
